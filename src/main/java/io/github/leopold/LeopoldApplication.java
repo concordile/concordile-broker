01ff8b8a409c892d71296d8f0cc762f39e30bea5
Grig Alex <alexengrigdev@gmail.com>
2023-01-30 09:08:09 +0300

    Init project

---
+++ src/main/java/io/github/leopold/LeopoldApplication.java
@@ -0,0 +1,13 @@
+package io.github.leopold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeopoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeopoldApplication.class, args);
+    }
+
+}
