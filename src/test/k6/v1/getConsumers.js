7716f0d844b3ef2632b51a3dd556372a8431b600
Grig Alex <alexengrigdev@gmail.com>
2023-08-05 22:25:34 +0300

    Add k6 test for /api/v1/consumers

---
+++ src/test/k6/v1/getConsumers.js
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2023 The Project Leopold Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import http from 'k6/http'
+import { sleep } from 'k6'
+
+export const options = {
+    vus: 100,
+    duration: '60s'
+}
+
+export default function () {
+    http.get('http://localhost:1975/api/v1/consumers')
+    sleep(1)
+}
